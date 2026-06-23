765bcdaa532b8815ff6368c10438928a2158645b
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-06-23 20:03:23 +0300

    wallet and user models

--- src/main/java/com/amiryousef/wallet/models/User.java
+++ src/main/java/com/amiryousef/wallet/models/User.java
@@ -3,7 +3,88 @@ package com.amiryousef.wallet.models;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "users")
 public class User {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created_at")
+    private Date created_at;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String password, Date created_at) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.created_at = created_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", created_at=" + created_at +
+                '}';
+    }
 }
--- src/main/java/com/amiryousef/wallet/models/Wallet.java
+++ src/main/java/com/amiryousef/wallet/models/Wallet.java
@@ -2,7 +2,32 @@ package com.amiryousef.wallet.models;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "wallets")
 public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "balance")
+    private Double balance;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "version")
+    private int version;
+
+    @Column(name = "date")
+    private Date date;
 }
