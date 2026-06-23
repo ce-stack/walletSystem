2e0750e8908dd7463890d94251b79d2c33fa6457
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-06-23 16:11:53 +0300

    first commit

---
+++ src/main/java/com/amiryousef/wallet/WalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.amiryousef.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletSystemApplication.class, args);
+	}
+
+}
