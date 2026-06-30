00f9b97b8db94a2d46438f16b3474fee56cef9a1
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-06-30 16:08:47 +0300

    create two fake users for testing

--- src/main/resources/db/migration/V7__insert_users_testing.sql
+++ src/main/resources/db/migration/V7__insert_users_testing.sql
@@ -1,3 +1,6 @@
 INSERT INTO public.users (id, email, name, password, created_at)
-VALUES (1, 'amir_test@gmail.com', 'Amir', '123456', now())
+
+VALUES
+    (1, 'amir_test_one@gmail.com', 'Amir wallet one', '123456', now()),
+    (2, 'amir_test_two@gmail.com', 'Amir wallet two', '123456', now())
     ON CONFLICT (id) DO NOTHING;
